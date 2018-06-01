951f2180b84cb0e590f9ee871b7cffe190b2e1ee
Alberto Pinardi <pinardi.alberto@gmail.com>
2018-06-01 08:51:54 +0200

    Added error message on parameter not set

--- reboot.sh
+++ reboot.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-gammu sendsms TEXT $1 -text "Inizio reboot"
-reboot
+if [ -Z  $1     ]; then
+    printf "Dst number not set. Use like this  \n ~ #./reboot.sh +393330101010101 \n"
+else
+    gammu sendsms TEXT $1 -text "Inizio reboot programmato - "
+    reboot
+    fi
