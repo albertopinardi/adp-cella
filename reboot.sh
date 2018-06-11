19e7cd2932753c4233b7881c48c9c3a5af2a420a
Alberto Pinardi <pinardi.alberto@gmail.com>
2018-06-11 10:46:22 +0200

    Minor Changes to sms text

--- reboot.sh
+++ reboot.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-if [ -Z  $1     ]; then
+txt="Inizio reboot programmato - "
+dt=$(date)
+txt="$txt $dt"
+if [ -z $1 ]; then
     printf "Dst number not set. Use like this  \n ~ #./reboot.sh +393330101010101 \n"
 else
-    gammu sendsms TEXT $1 -text "Inizio reboot programmato - "
+    gammu sendsms TEXT $1 -text $txt
     reboot
     fi
