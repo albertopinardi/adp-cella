bb5d6208729b0f1d1a0e4a2125537af7b7200285
Alberto Pinardi <pinardi.alberto@gmail.com>
2018-12-19 16:06:39 +0100

    Enanched reboot script

--- reboot.sh
+++ reboot.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-txt="Inizio reboot programmato - "
-dt=$(date)
-txt="$txt $dt"
+txt="Inizio reboot programmato -"
+dt="$(date)"
+unix="$(date +%s)"
+let unix-=86400
+avg=$(sqlite3 ~/datalogger.db "select avg(temp_freezer) from records where time > $unix;")
+tavg="- Temperatura media del giorno $avg"
+tt="$txt $dt $tavg"
 if [ -z $1 ]; then
-    printf "Dst number not set. Use like this  \n ~ #./reboot.sh +393330101010101 \n"
+    printf "Destination number not set. Usage example:  \n ~ #./reboot.sh +393330101010101 \n"
 else
-    gammu sendsms TEXT $1 -text $txt
+    gammu sendsms TEXT $1 -text "$tt"
     reboot
     fi
