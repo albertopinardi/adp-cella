4357250834c672aec49bded9c2a37f985bac8811
Alberto Pinardi <pinardi.alberto@gmail.com>
2019-09-19 16:07:59 +0200

    stable version

--- parser.sh
+++ parser.sh
@@ -1,20 +1,30 @@
-
 #!/bin/bash
 #Alberto Pinardi
 
 SMS_DIR="/var/spool/gammu/inbox/"
 NUM="3406694374"
 
-function send_temp(){
-    dt="$(date)"
-    unix="$(date +%s)"
-    let unix-3600
-    avg=$(sqlite3 /root/datalogger.db "select avg(temp_freezer) from records where time > $unix;")
-    tavg="Temperatura media dell'ultima ora $avg"
-    tt="$dt $tavg"
-    gammu sendsms TEXT "+393406694374" -text "$tt"
+function date_selector(){
+        dt_h="$(date +%Y%m%d_%H)"
+        min=$(date +%M)
+        if (( min <= 30 ));
+        then
+                dt_c=$dt_h"00"
+        else
+                dt_c=$dt_h"30"
+        fi
+        echo $dt_h
 }
 
+function send_temp(){
+        dt="$(date)"
+        punix="$(date +%s)"
+        let unix=punix-3600
+        avg=$(sqlite3 ~/datalogger.db "select avg(temp_freezer) from records where time > $unix;")
+        tavg="Temperatura media dell'ultima ora $avg"
+        tt="$dt $tavg"
+        gammu sendsms TEXT "$NUM" -text "$tt"
+}
 
 function selector(){
         case $1 in
@@ -22,12 +32,12 @@ function selector(){
         "temp") send_temp;;
         *) echo "Clean...";;
         esac
-        
 }
 
-for i in $(ls -t $SMS_DIR | grep $NUM ) ;do
-        printf "Messaggio : "
-        cat $SMS_DIR$i
+interval=$(date_selector)
+for i in $(ls -t $SMS_DIR | grep "IN"$interval |grep $NUM ) ;do
+#       echo "Messaggio : "
+#       cat $SMS_DIR$i
         TEXT=$(cat $SMS_DIR$i)
         selector $TEXT
         echo
